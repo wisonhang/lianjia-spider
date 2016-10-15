c33fcc6ea3b3079f5397690613e7a3b1da5f4279
wisonhang <hangdawei@126.com>
2016-10-15 14:06:48 +0800

    Update lj_spider.py

--- lj_spider.py
+++ lj_spider.py
@@ -19,16 +19,51 @@ import time
 from numpy import *
 from pandas import *
 
-from log_info.bj import *  #导入登录信息  北京
+from log_info.bj import *  #导入登录信息  北京  
 #from log_info.hz import * #导入登录信息  杭州
 #from log_info.sz import * #导入登录信息  深圳
 #from log_info.gz import * #导入登录信息  广州
 
-######################北京链家###
+######################北京链家 导入示例###
+'''
+cookies={
+    'lianjia_uuid':'99949991-e9c7-4df8-b8bc-7d427ccf98ad',
+    '_ga':'GA1.2.1956483913.1473815618',
+    'gr_user_id':'84a5ae61-9280-4505-b0a2-ebdca807fe32',
+    'ubta':'2299869246.3823483042.1473815618645.1475972328299.1475972333450.185',
+    'pt_393d1d99':'uid=El7-zd3kPbVUnu-qmjjIng&nid=0&vid=COojRQz1TWAI6doA/t-01w&vn=1&pvn=6&sact=1474799546672&to_flag=0&pl=lI2h1cVGgrwUeTf-92t3kQ*pt*1474799418062', 
+    '_smt_uid':'57f5eb50.334841a2', 'a7122_times':'1', 'CNZZDATA1253477573':'1879280474-1475798825-%7C1475971762', 
+    'CNZZDATA1254525948':'1826278568-1475800458-%7C1475973258', 'CNZZDATA1255633284':'1268179366-1475796095-%7C1475974302', 
+    'select_city':'110000', 'cityCode':'bj', 'ubt_load_interval_b':'1475929188945', 
+    'ubtc':'2299869246.3823483042.1475972333459.6C28FC5109B0462DADAD43CCC12812E0', 'ubtd':'52', 'lianjia_token':'2.0024eab9f55df1cf6c354790c4f2400ad7', 
+    '_jzqa':'1.1393651109478892300.1475901567.1475903927.1475910449.3', '_jzqc':'1', '_jzqckmp':'1', 
+    'all-lj':'3e56656136803bc056cf7a329e54869e', 'lianjia_ssid':'beef9d5f-44c0-49da-abe5-cea4d7e53a5d', 
+    'CNZZDATA1255604082':'1589290616-1475971693-%7C1475971693'    
+}
+headers={
+'Host': 'bj.lianjia.com',
+'User-Agent':' Mozilla/5.0 (Windows NT 6.3; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+'Accept-Encoding': 'gzip, deflate',
+'Referer': 'http://bj.lianjia.com/chengjiao/',
+'Connection': 'keep-alive',
+'Upgrade-Insecure-Requests': '1',
+'Cache-Control': 'max-age=0'
+}
+
+
+#北京区域列表
+regionsb=["东城","西城","朝阳","海淀","丰台","石景山","通州","昌平","大兴",
+"亦庄开发区","顺义","房山","门头沟","平谷","怀柔","密云","延庆","燕郊"]
+
+xiaoqulink="http://bj.lianjia.com/xiaoqu/rs"
+xiaoqulink1="http://bj.lianjia.com/xiaoqu/pg%srs%s/" 
+chengjiaolink="http://bj.lianjia.com/chengjiao/rs"
+chengjiaolink1="http://bj.lianjia.com/chengjiao/pg%srs%s/"
+'''
 ####################################################
 lock = threading.Lock()
-
-
 class SQLiteWraper(object):
     """
     数据库的一个小封装，更好的处理多线程写入
@@ -113,7 +148,7 @@ def xiaoqu_spider(db_xq,url_page="http://bj.lianjia.com/xiaoqu/"):
     try:
         s=requests.session()
         r=s.post(url_page,headers=headers,cookies=cookies,timeout=60)
-        soup=BeautifulSoup(r.content.decode('utf-8','ignore'),'lxml')
+        soup=BeautifulSoup(r.content.decode('utf-8','ignore'),'lxml')  #此处 decode 需要加 ignore 
         xiaoqu_list=soup.findAll('li',attrs={'class':'clear'})
     except Exception as e:
         print (e)
@@ -149,11 +184,11 @@ def xiaoqu_spider(db_xq,url_page="http://bj.lianjia.com/xiaoqu/"):
         command=gen_xiaoqu_insert_command(info_dict)
         db_xq.execute(command,1)
 
-def do_xiaoqu_spider(db_xq,region="朝阳"):
+def do_xiaoqu_spider(db_xq,region="朝阳",base_url=xiaoqulink,page_url=xiaoqulink1):
     """
     爬取大区域中的所有小区信息
     """
-    url=xiaoqulink+region+"/"
+    url=base_url+region+"/"
     try:
         s=requests.session()
         r=s.get(url,headers=headers,cookies=cookies,timeout=60)
@@ -166,7 +201,7 @@ def do_xiaoqu_spider(db_xq,region="朝阳"):
     else:
         threads=[]
         for i in range(total_pages):
-            url_page=xiaoqulink1%(i+1,region)
+            url_page=page_url%(i+1,region)
         #print(url_page)
             t=threading.Thread(target=xiaoqu_spider,args=(db_xq,url_page))
             threads.append(t)
@@ -197,7 +232,7 @@ def gen_chengjiao_insert_command(info_dict):
 
 def chengjiao_spider(db_cj,db_xq,area,page,url=chengjiaolink1):
     """
-    爬取页面链接中的成交记录
+    爬取页面链接中的成交记录 url="http://xx.lianjia.com/chengjiao/pg%srs%s/" 
     """
     time.sleep(20)
     try:
@@ -211,7 +246,6 @@ def chengjiao_spider(db_cj,db_xq,area,page,url=chengjiaolink1):
         print (e)
         exception_write('chengjiao_spider',url_page)
         return 
-    #cj_list=soup.findAll('ul',{'class':'listContent'})[0].findAll('li')
     for cj in cj_list:
         info_dict={}
         href=cj.find('a')
@@ -266,7 +300,7 @@ def chengjiao_spider(db_cj,db_xq,area,page,url=chengjiaolink1):
         
 def xiaoqu_chengjiao_spider(db_cj,db_xq,xq_name="朝阳",url_base=chengjiaolink):
     """
-    爬取小区成交记录
+    爬取小区成交记录  url_base="http://xx.lianjia.com/chengjiao/rs"
     """
     url=url_base+urllib.parse.quote(xq_name)+"/"
     try:
@@ -286,8 +320,6 @@ def xiaoqu_chengjiao_spider(db_cj,db_xq,xq_name="朝阳",url_base=chengjiaolink)
         total_pages=int(ceil(float(num)/30))
         threads=[]
         for i in range(total_pages):
-        #url_page=cj1 % (i+1,xq_name)
-        #t=threading.Thread(target=chengjiao_spider,args=(db_cj,db_xq,url_page))
             page=i+1
             t=threading.Thread(target=chengjiao_spider,args=(db_cj,db_xq,xq_name,page))
             threads.append(t)
@@ -312,11 +344,7 @@ def do_xiaoqu_chengjiao_spider(db_cj,db_xq,command="select distict name from xia
     
     
 def xiaoqu_chenjiao_update(db_cj,db_xq,con="select district, count(road) from chengjiao GROUP by district",url_base=chengjiaolink):
-    #con=sqlite3.connect('sh-chengjiao.db')
-    #sql="select * from chengjiao"
-    #data=read_sql(sql,con)
     data=db_cj.fetchall(con)
-    #for xq_name,val in zip(data.groupby(['district']).count()[['name']].index,data.groupby(['district']).count()['name'].values):
     for xq_name,val in data:
         url=url_base+urllib.parse.quote(xq_name)+"/"
         try:
@@ -401,32 +429,6 @@ def exception_spider(db_cj,db_xq):
     
     
     
-#########异常处理
-
-
-
-def a1(dataframe,col='',threshold=.95):
-    d=dataframe[col]
-    dataframe[col+'isAnomaly']= (d>d.quantile(threshold)) | (d< d.quantile(1-threshold))
-    return(dataframe)
-
-
-
-def a2(dataframe,col='',threshold=3.5):
-
-    d=dataframe[col]
-    zscore=(d-d.mean())/d.std()
-    dataframe[col+'isAnomaly']= (zscore.abs()> threshold) | (zscore.abs()< -threshold)
-    return(dataframe)
-
-def a3(dataframe,col='',threshold=3.5):
-
-    dd=dataframe[col]
-    MAD=(dd -dd.median()).abs().median()
-    zscore=((dd -dd.median())*0.6475 /MAD).abs()
-    dataframe[col+'isAnomaly']= (zscore > threshold) | (zscore <- threshold)
-    return(dataframe)
-
 
 
 ########################################################################
